26c8d02e82ba9721380d2ae13cd0df4e771d5336
Vanessa Ceballos <Vanessa@Vanessas-MacBook-Pro.local>
2015-11-11 12:40:21 -0800

    adding pagination shortcode to press

--- press.php
+++ press.php
@@ -102,14 +102,17 @@ function pressLoop( $atts ) {
           'type' => 'press',
       ), $atts ) );
       $output = '';
+      $paged = ( get_query_var('page') ) ? get_query_var('page') : 1;
       $args = array(
           'post_type' => $type,
-          'sort_column'   => 'menu_order'
+          'sort_column'   => 'menu_order',
+          'posts_per_page' => 10,
+          'paged' => $paged
       );
       $yo_quiery = new WP_Query( $args );
+
       while ( $yo_quiery->have_posts() ) : $yo_quiery->the_post();
-          $output .=
-          '<div class="row press">
+          $output .= '<div class="row press">
               <div class="col-lg-12">
               <div class="media-left hidden-xs">
                 <a class="postpic" href="'.get_post_meta( get_the_ID(), 'link', true ).'">
@@ -127,8 +130,50 @@ function pressLoop( $atts ) {
               </div>
             </div>';
       endwhile;
-      wp_reset_query();
-      return $output;
+
+    wp_reset_query();
+    return $output;
+
   }
   add_shortcode('press-loop', 'pressLoop');
+
+
+
+
+  function press_pagination( $atts ) {
+    extract( shortcode_atts( array(
+        'type' => 'press',
+    ), $atts ) );
+    $paged = ( get_query_var('page') ) ? get_query_var('page') : 1;
+    $args = array(
+        // 'post_parent' => $parent,
+        'post_type' => $type,
+        'sort_column'   => 'menu_order',
+        'posts_per_page' => 10,
+        'paged' => $paged
+    );
+    $yo_quiery = new WP_Query( $args );
+
+    $yo_quiery->query_vars['paged'] > 1 ? $current = $yo_quiery->query_vars['paged'] : $current = 1;
+
+    $pagination = array(
+        'base' => @add_query_arg('page','%#%'),
+        'format' => '',
+        'total' => $yo_quiery->max_num_pages,
+        'current' => $current,
+        'show_all' => true,
+        'type' => 'list',
+        'next_text' => '&raquo;',
+        'prev_text' => '&laquo;'
+    );
+
+    if( !empty($yo_quiery->query_vars['s']) )
+        $pagination['add_args'] = array( 's' => get_query_var( 's' ) );
+
+
+    wp_reset_query();
+    return paginate_links( $pagination );
+  }
+  add_shortcode('paginate-press', 'press_pagination');
+
 ?>
